2222c957640a2d059b399ec29af5ecc186b4c01e
tolojo <73295309+tolojo@users.noreply.github.com>
2021-11-07 18:20:28 +0000

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-const connectionString = "postgres://postgres:postgres@localhost:5432/mt"
+const connectionString = "postgres://qqjwtukuubzaiq:79d5d5d6c2260c226ba05241dda237583846b6163a75f90614eb18c362517e4f@ec2-3-214-121-14.compute-1.amazonaws.com:5432/d6rec0os8s84nc"
 
 const Pool = pg.Pool
 const pool = new Pool({
